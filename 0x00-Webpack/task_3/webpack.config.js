26b4a0e5fa677ea371e4bc0dadbd03edc16bf865
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-21 02:49:45 +0300

    Add task 3 files

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      }
+    ]
+  }
+};
