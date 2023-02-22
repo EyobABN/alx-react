0cd6244db2a3cf0a42eff048cd3fb0ada9e07ef4
EyobABN <eyob.alamerew.belay@gmail.com>
2023-02-22 20:07:07 +0300

    Add task 2 file

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from './Holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img className="logo" src={logo} alt="logo"></img>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email"></input>
+        <label for="pass">Password</label>
+        <input type="password" id="pass" name="password"></input>
+        <button type="button">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()}- {getFooterCopy(false)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
